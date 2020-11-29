c5580a0e9a5e2fa4d4d53b9d9abaaad5d96bb6d0
SebastianSinozic <55134663+SebastianSinozic@users.noreply.github.com>
2020-11-29 16:16:01 +0100

    Add files via upload

---
+++ sinozicSebastianKolekcije/sinozicSebastianKolekcije/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+namespace ArrayListKolekcija
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList al = new ArrayList();
+            // Dodajemo elemente u kolekciju
+            al.Add(4);
+            al.Add(“C# i .NET”);
+            al.Insert(1, “Programiranje”);
+            
+            Console.WriteLine(“Elementi kolekcije na početku:”);
+            for (int i = 0; i < al.Count; i++)
+            {
+                Console.WriteLine(al[i]);
+            }
+            // Uklanjamo elemente iz kolekcije
+            if (al.Contains(4))
+            {
+                al.Remove(4);
+            }
+            int pozicija = al.IndexOf(“Programiranje”);
+            if (pozicija >= 0)
+            {
+                al.RemoveAt(pozicija);
+            }
+            Console.WriteLine(“\n-- Elementi kolekcije na
+            završetku:”);
+
+                 
+            for (int i = 0; i < al.Count; i++)
+            {
+                Console.WriteLine(al[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ sinozicSebastianKolekcije2/sinozicSebastianKolekcije2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+namespace sinozicSebastianKolekcije2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> brojevi = new List<int>();
+            // Program traži unos cijelih brojeva
+            while (true)
+            {
+                Console.Write(“Unesite cijeli broj:”);
+                int broj = int.Parse(Console.ReadLine());
+                // Ako se unese 0, prekidamo unos
+                if (broj == 0)
+                {
+                    break;
+                }
+                // Dodajemo broj u listu
+                brojevi.Add(broj);
+            }
+            brojevi.Sort(); // sortiramo elemente liste
+            brojevi.Reverse(); // obrćemo redoslijed elemenata u listi
+            
+        // Ispisujemo elemente liste
+            Console.WriteLine(“Uneseni brojevi pad redoslijedom:”);
+            foreach (int broj in brojevi)
+            {
+                Console.Write (“ { 0 }, “, broj );
+            }
+            Console.ReadKey();
+        }
+    }
+}
